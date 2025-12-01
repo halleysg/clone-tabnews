f9c675fa622769ae72bc6319b8f4cb748a652352
Halley S. Guimarães <halley.sguimaraes@gmail.com>
2025-12-01 17:01:41 -0300

    add `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
+
   res.status(200).json({ message: "API is running" });
 }
 
