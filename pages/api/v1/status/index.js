58ae12fc40a6fe5aa30614f8dde4d5b902bef149
Halley S. Guimarães <halley.sguimaraes@gmail.com>
2025-12-02 19:34:43 -0300

    make `database.js` more robust to error handling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ message: "API is running" });
+  const version = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+
+  const maxConnections = (await database.query("SHOW max_connections;")).rows[0]
+    .max_connections;
+
+  const openConnections = (
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [process.env.POSTGRES_DB],
+    })
+  ).rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        open_connections: openConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+
+  expect(resBody.dependencies.database.version).toEqual("16.0");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.open_connections).toEqual(1);
+
+  expect(new Date(resBody.updated_at).toISOString()).toEqual(
+    resBody.updated_at,
+  );
 });
