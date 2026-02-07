3a52a50e2540675d1aba26eb3859afa2a70c2e84
Halley S. Guimarães <halley.sguimaraes@gmail.com>
2026-02-07 09:58:59 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,42 +1,37 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller";
 
-async function status(req, res) {
-  try {
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const version = (await database.query("SHOW server_version;")).rows[0]
-      .server_version;
+router.get(getHandler);
 
-    const maxConnections = (await database.query("SHOW max_connections;"))
-      .rows[0].max_connections;
+export default router.handler(controller);
 
-    const openConnections = (
-      await database.query({
-        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-        values: [process.env.POSTGRES_DB],
-      })
-    ).rows[0].count;
+async function getHandler(req, res) {
+  const updatedAt = new Date().toISOString();
 
-    res.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: version,
-          max_connections: parseInt(maxConnections),
-          open_connections: openConnections,
-        },
+  const version = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+
+  const maxConnections = (await database.query("SHOW max_connections;")).rows[0]
+    .max_connections;
+
+  const openConnections = (
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [process.env.POSTGRES_DB],
+    })
+  ).rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        open_connections: openConnections,
       },
-    });
-  } catch (e) {
-    const publicErrorObject = new InternalServerError({
-      cause: e,
-    });
-
-    console.error(publicErrorObject);
-
-    res.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
